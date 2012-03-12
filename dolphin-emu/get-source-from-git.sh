7ca9f00d74763abafc13c5388e23dfd156a26e6f
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-03-12 13:04:55 -0400

    Fix dolphin-emu get-source-from-git.sh script

--- dolphin-emu/get-source-from-git.sh
+++ dolphin-emu/get-source-from-git.sh
@@ -6,7 +6,9 @@ SOURCEDIR=$(cd $(dirname ${0}) | pwd)
 pushd ${TEMPDIR}
 
 git clone https://code.google.com/p/dolphin-emu/ dolphin-emu-3.0
+cd dolphin-emu-3.0
 git checkout 3.0
+cd ..
 tar -Jcv --exclude-vcs -f dolphin-emu-3.0.tar.xz dolphin-emu-3.0
 cp dolphin-emu-3.0.tar.xz ${SOURCEDIR}/
 
